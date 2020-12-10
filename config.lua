cea759c005f568d71521b650c3621434588ab052
BuildTools <unconfigured@null.spigotmc.org>
2020-12-10 13:57:36 -0300

    Msg

--- config.lua
+++ config.lua
@@ -11,7 +11,7 @@ if args[1] == "setOrigin" then
     settings.set("x", 0)
     settings.set("y", 0)
     settings.set("z", 0)
-
+    Write_Screen("Set current position as origin!\n")
 elseif args[1] == "setWidth" then
     local w = tonumber(args[2])
     if not w then
