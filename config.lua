10c3c1a3f76ee137d197e30890e37f066f539f0c
BuildTools <unconfigured@null.spigotmc.org>
2020-12-10 13:29:13 -0300

    Fix #2

--- config.lua
+++ config.lua
@@ -1,40 +1,35 @@
 local args = {...}
 
+local function Write_Screen(string)
+    term.clear()
+    term.setCursorPos(1,1)
+    term.write("Set Origin!!\n")
+    term.setCursorPos(1,2)
+end
+
 if args[1] == "setOrigin" then
     settings.set("x", 0)
     settings.set("y", 0)
     settings.set("z", 0)
-    term.clear()
-    term.setCursorPos(1,1)
-    term.write("Set Origin!!\n")
+
 elseif args[1] == "setWidth" then
     local w = tonumber(args[2])
     if not w then
-        term.clear()
-        term.setCursorPos(1,1)
-        term.write("Invalid width!\n")
+        Write_Screen("Invalid width!\n")
     else
         w = math.floor(w)
         settings.set("w", w)
-        term.clear()
-        term.setCursorPos(1,1)
-        term.write("Set width to " .. w.."\n")
+       Write_Screen("Set width to " .. w.."\n")
     end
 elseif args[1] == "setHeight" then
     local h = tonumber(args[2])
     if not h then
-        term.clear()
-        term.setCursorPos(1,1)
-        term.write("Invalid height!\n")
+        Write_Screen("Invalid height!\n")
     else
         h = math.floor(h)
-        settings.set("h", w)
-        term.clear()
-        term.setCursorPos(1,1)
-        term.write("Set height to " .. h.."\n")
+        settings.set("h", h)
+        Write_Screen("Set height to " .. h.."\n")
     end
 else
-    term.clear()
-    term.setCursorPos(1,1)
-    term.write("Invalid usage!\n")
+    Write_Screen("Invalid usage!")
 end
