2e35a62bedc96bfb2df4e604adbf83c4c928788b
BuildTools <unconfigured@null.spigotmc.org>
2020-12-10 18:16:47 -0300

    Config Reset

--- config.lua
+++ config.lua
@@ -9,7 +9,6 @@ end
 
 if args[1] == "setOrigin" then
     settings.set("x", 0)
-    settings.set("y", 0)
     settings.set("z", 0)
     Write_Screen("Set current position as origin!\n")
 elseif args[1] == "setWidth" then
@@ -30,6 +29,16 @@ elseif args[1] == "setHeight" then
         settings.set("h", h)
         Write_Screen("Set height to " .. h.."\n")
     end
+elseif args[1] == "reset" then
+    settings.set("x", 0)
+    settings.set("z", 0)
+    settings.set("w", 20)
+    settings.set("h", 10)
+    settings.set("state", 0)
+    settings.set("currentOrientation", 1)
+    settings.set("currentDirection", 1)
+    settings.set("wantedOrientation", 1)
+    Write_Screen("Reset!!")
 else
     Write_Screen("Invalid usage!")
 end
