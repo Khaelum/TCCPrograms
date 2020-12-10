2d13bb8629782eb46ab5dac527c37ab80ac00860
BuildTools <unconfigured@null.spigotmc.org>
2020-12-10 12:42:55 -0300

    initial commit

---
+++ farmer.lua
@@ -0,0 +1,35 @@
+local term = term
+local settings = settings
+
+
+local iX = settings.get("x")
+local iY = settings.get("y")
+local iZ = settings.get("z")
+
+local bFirstTime = false
+
+if not iX or iY or iZ then bFirstTime = true end
+
+
+
+
+local function set_up()
+    term.clear()
+    term.setCursorPos(1,1)
+    term.write("Input farming area length\n")
+    local input = read()
+    input = tonumber(input)
+    if input then input = input % 1 end
+    term.write(input)
+end
+
+
+--[[
+    Set-up process takes:
+    origin (initial turtle position)
+    rect width
+    rect height
+
+    stores information so as to restart what it was doing before in case of disruptions
+
+]]
